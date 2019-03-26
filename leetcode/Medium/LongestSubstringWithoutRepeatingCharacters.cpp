5c1ceb02817e28374190a8b98e5c70ae1167c8ed
ZnyY <3081070036@qq.com>
2019-03-26 21:32:30 +0800

    updd

--- leetcode/Medium/LongestSubstringWithoutRepeatingCharacters.cpp
+++ leetcode/Medium/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,18 +1,22 @@
-public class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        Set<Character> set = new HashSet<>();
-        int ans = 0, i = 0, j = 0;
-        while (i < n && j < n) {
-            // try to extend the range [i, j]
-            if (!set.contains(s.charAt(j))){
-                set.add(s.charAt(j++));
-                ans = Math.max(ans, j - i);
-            }
-            else {
-                set.remove(s.charAt(i++));
+#include <bits/stdc++.h>
+using namespace std ;
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        set<char> st ;
+        int i = 0 , j = 0 , n = s.size() ;
+        int ans = 0 ;
+        while (j < n){
+            if (!st.count(s[j])){
+                st.insert(s[j++]) ;
+                ans = max (ans , j - i) ;
+            }else{
+                st.erase(s[i++]) ;
             }
         }
-        return ans;
+        return ans ;
     }
+}A;
+int main(){
+    cout << A.lengthOfLongestSubstring("pwwkew") ;
 }
